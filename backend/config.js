9674c5e78864afb540ac5989abac819b69089e14
besufkad-ayele <ayebesufkad@gmail.com>
2024-01-28 13:05:20 +0300

    updating in the api

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:1234@book-store.arsnodx.mongodb.net/books-collection?retryWrites=true&w=majority';
+  '';
+  // mongodb+srv://password:1234@cafe-store.arsnodx.mongodb.net/cafe-collection?retryWrites=true&w=majority
+  // mongodb+srv://<password>:1234@<database>.arsnodx.mongodb.net/<add your own collection then?retryWrites=true&w=majority
